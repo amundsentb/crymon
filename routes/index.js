3fb78d17a823a2b469179e0daa64c76768ee28eb
Thomas Brecke Amundsen <amundsentb@gmail.com>
2021-04-30 23:26:05 +0200

    testing

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,44 @@
 var express = require('express');
 var router = express.Router();
+var request = require("request");
+
+const dotenv = require("dotenv");
+dotenv.config();
+
+const USER = process.env.RPC_USER;
+const PASS = process.env.RPC_PASSWORD;
+
+const headers = {
+  "content-type": "text/plain;"
+};
+
+router.get("/test", (req, res) => res.json({ msg: "backend works" }));
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+
+router.get("/getblockcount", (req, res) => {
+  var dataString = `{"jsonrpc":"1.0","id":"curltext","method":"getblockcount","params":[]}`;
+  var options = {
+    url: `http://${USER}:${PASS}@127.0.0.1:8332/`,
+    method: "POST",
+    headers: headers,
+    body: dataString
+  };
+  
+  callback = (error, response, body) => {
+    if (!error && response.statusCode == 200) {
+      const data = JSON.parse(body);
+      res.send(data);
+    }
+  };
+  request(options, callback);
+});
+
+
+
 
 module.exports = router;
